c62ee16e991c17a961b9a026c0ef9806f2a0691f
Viktoras Agejevas <v.agejevas@gmail.com>
2015-06-23 18:42:43 +0300

    Update Mergesort scaladoc

--- src/main/scala/algo/MergeSort.scala
+++ src/main/scala/algo/MergeSort.scala
@@ -5,6 +5,13 @@ import Ordering.Implicits._
 /**
  * Recursive Merge sort implementation.
  *
+ * <ol>
+ *   <li>Divide the unsorted list into n sublists, each containing 1 element
+ *    (a list of 1 element is considered sorted).</li>
+ *   <li>Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist
+ *    remaining. This will be the sorted list.</li>
+ * </ol>
+ *
  * @author Viktoras Agejevas
  */
 class MergeSort {
